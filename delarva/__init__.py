6710b8f96c69a8e7471f47da9fee0a288e0c12a3
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-02-11 16:07:45 +0000

    chore: add meta files

---
+++ delarva/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: MIT
+"""
+Delarva
+=======
+A slash-based debug cog for nextcord.
+:copyright: (c) 2023-present ooliver1
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "delarva"
+__author__ = "ooliver1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present ooliver1"
+__version__ = "0.1.0"
